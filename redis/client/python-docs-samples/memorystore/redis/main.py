c8e4f0fbca3d800aa58cf3b76869baed00e154a6
Tiancan Yu <tiancan@Ys-MacBook-Pro.local>
2020-03-27 19:49:28 -0400

    update

--- redis/client/python-docs-samples/memorystore/redis/main.py
+++ redis/client/python-docs-samples/memorystore/redis/main.py
@@ -19,8 +19,10 @@ import redis
 
 app = Flask(__name__)
 
-redis_host = os.environ.get('REDISHOST', 'localhost')
-redis_port = int(os.environ.get('REDISPORT', 6379))
+# redis_host = os.environ.get('REDISHOST', 'localhost')
+# redis_port = int(os.environ.get('REDISPORT', 6379))
+redis_host = 'localhost'
+redis_port = 6379
 redis_client = redis.StrictRedis(host=redis_host, port=redis_port)
 
 
